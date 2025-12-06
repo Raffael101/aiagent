332599af76555e47d0d80e8d0de4cbd301836dca
Raffael101 <239906373+Raffael101@users.noreply.github.com>
2025-12-06 14:25:21 +0800

    fixed token counter

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ if api_key is None:
     raise RuntimeError("API key missing!")
 client = genai.Client(api_key=api_key)
 response = client.models.generate_content(model="gemini-2.5-flash", contents=content)
-if response.usage_metadata() is None:
+if not response.usage_metadata:
     raise RuntimeError("Missing Usage Data")
 else:
     print(f"User prompt: {content}")
-    print(f"Prompt tokens: {response.prompt_token_count}")
-    print(f"Response tokens: {response.candidates_token_count}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(response.text)
 def main():
     print("Hello from aiagent!")
