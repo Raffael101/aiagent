ba1239d380b3b265b2650cda643959f137a3f074
Raffael101 <239906373+Raffael101@users.noreply.github.com>
2025-12-07 16:50:58 +0800

    5: prompt is now argument in CLI

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
 
-content = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-
 
 load_dotenv()
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("prompt", type=str, help="User prompt")
+args = parser.parse_args()
 api_key = os.environ.get("GEMINI_API_KEY")
 if api_key is None:
     raise RuntimeError("API key missing!")
 client = genai.Client(api_key=api_key)
-response = client.models.generate_content(model="gemini-2.5-flash", contents=content)
+response = client.models.generate_content(model="gemini-2.5-flash", contents=args.prompt)
 if not response.usage_metadata:
     raise RuntimeError("Missing Usage Data")
 else:
-    print(f"User prompt: {content}")
+    print(f"User prompt: {args.prompt}")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(response.text)
