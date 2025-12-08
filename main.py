4d6a1d6f1b6912d096311adde518da8e08cb4b63
Raffael101 <239906373+Raffael101@users.noreply.github.com>
2025-12-08 11:48:27 +0800

    6: added verbose argument

--- main.py
+++ main.py
@@ -2,23 +2,30 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+
 
 
 load_dotenv()
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 args = parser.parse_args()
 api_key = os.environ.get("GEMINI_API_KEY")
 if api_key is None:
     raise RuntimeError("API key missing!")
 client = genai.Client(api_key=api_key)
-response = client.models.generate_content(model="gemini-2.5-flash", contents=args.prompt)
+messages = [types.Content(role="user", parts=[types.Part(text=args.prompt)])]
+response = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
+
 if not response.usage_metadata:
     raise RuntimeError("Missing Usage Data")
-else:
+elif args.verbose:
     print(f"User prompt: {args.prompt}")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(response.text)
+else:
     print(response.text)
 def main():
     print("Hello from aiagent!")
