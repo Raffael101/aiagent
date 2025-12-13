59d2f3be6f4bba5f9e3719615333b85056063685
Raffael101 <239906373+Raffael101@users.noreply.github.com>
2025-12-13 12:05:49 +0800

    added write to file

---
+++ functions/write_file.py
@@ -0,0 +1,24 @@
+import os
+
+def write_file(working_directory, file_path, content):
+    work_dir = os.path.abspath(working_directory)
+    tar_dir = os.path.abspath(os.path.join(working_directory, file_path))
+    if not tar_dir.startswith(work_dir):
+        return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+    elif os.path.exists(tar_dir) == False and os.path.exists(os.path.dirname(tar_dir)) == False:
+        with open(tar_dir, "w") as f:
+            f.write(content)
+        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+    elif os.path.exists(tar_dir) == False:
+        directory = os.path.dirname(tar_dir)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        with open(tar_dir, "w") as f:
+            f.write(content)
+        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+    elif os.path.exists(tar_dir):
+        with open(tar_dir, "w") as f:
+            f.write(content)
+        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+    else:
+        raise Exception("Error: something wrong with the if statement")
