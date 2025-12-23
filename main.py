40a5e18ec992669557e71865904f0762f1bf6707
Raffael101 <239906373+Raffael101@users.noreply.github.com>
2025-12-23 08:48:09 +0800

    17: update.. i hate google api... Christmas

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ from functions.run_python_file import schema_run_python_file, run_python_file
 from functions.write_file import schema_write_file, write_file
 from functions.call_funtion import call_function
 
-def has_function_calls(list)
 
 
 def generate_content(client, messages, verbose, prompt):
@@ -27,25 +26,27 @@ def generate_content(client, messages, verbose, prompt):
         print(f"User prompt: {prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    if not response.function_calls:
-        print(response.text)
-        return
     tool_results = []
     for function_call_part in response.function_calls:
         result = call_function(function_call_part, verbose=verbose)
         if not  result.parts or not result.parts[0].function_response.response:
             raise Exception("Error: No response found? i think")
-
         tool_results.append(result.parts[0])
         if verbose:
             print(f"-> {result.parts[0].function_response.response}")
+    has_function_calls = False
     try:
         messages.append(types.Content(role="user", parts=tool_results))
         for i in response.candidate:
-            if i.content.parts == response.function_call:
+            if not response.function_calls:
+                return response.text
+            final = generate_content(client, messages, args.verbose, args.prompt)    
+            if final:
                 print("Final response: ")
                 print(response.text)
                 break
+
+  
     except Exception as e:
         print("Error:", e)
 
